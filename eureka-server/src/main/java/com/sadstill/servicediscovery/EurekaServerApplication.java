c0ee052de2379db92d8a89812c5f2c50dcef4883
sadstill <yarik20052013@gmail.com>
2023-12-05 16:34:34 +0300

    added modules with microservices

---
+++ eureka-server/src/main/java/com/sadstill/servicediscovery/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.sadstill.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
